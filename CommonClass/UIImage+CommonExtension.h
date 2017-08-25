831aaafad14da55a1e46e039c7da54157607a364
blazer402@126.com <blazer402>
2017-08-25 21:42:51 +0800

    init

---
+++ CommonClass/UIImage+CommonExtension.h
@@ -0,0 +1,17 @@
+//
+//  UIImage+Extension.h
+//  CommonClass
+//
+//  Created by blazer on 2017/8/25.
+//  Copyright © 2017年 blazer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (CommonExtension)
+
+//切圆角
+- (UIImage *)circleImage;
+
+
+@end
---
+++ CommonClass/UIImage+CommonExtension.m
@@ -0,0 +1,34 @@
+//
+//  UIImage+Extension.m
+//  CommonClass
+//
+//  Created by blazer on 2017/8/25.
+//  Copyright © 2017年 blazer. All rights reserved.
+//
+
+#import "UIImage+CommonExtension.h"
+
+@implementation UIImage (CommonExtension)
+
+- (UIImage *)circleImage{
+    
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0);
+    
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    //设置一个范围
+    CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
+    //根据一个Rect创建一个椭圆
+    CGContextAddEllipseInRect(ctx, rect);
+    //裁剪
+    CGContextClip(ctx);
+    //将原照片画到图形上下文
+    [self drawInRect:rect];
+    //获得新图片
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+@end
