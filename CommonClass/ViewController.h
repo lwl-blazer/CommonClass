27d9aff4e02498f595267098aa331eb3b133f495
blazer402@126.com <blazer402>
2017-08-25 21:29:16 +0800

    Initial Commit

---
+++ CommonClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CommonClass
+//
+//  Created by blazer on 2017/8/25.
+//  Copyright © 2017年 blazer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
